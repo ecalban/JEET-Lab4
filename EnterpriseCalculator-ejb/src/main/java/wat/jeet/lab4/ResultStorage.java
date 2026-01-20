c59b90c2538e2ddbcf6373beddef884d20f3b3d9
Student <Student@10.7.10.70>
2026-01-20 09:15:15 +0100

    Finished successfully

---
+++ EnterpriseCalculator-ejb/src/main/java/wat/jeet/lab4/ResultStorage.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/J2EE/EJB40/SingletonEjbClass.java to edit this template
+ */
+package wat.jeet.lab4;
+
+import jakarta.ejb.Singleton;
+import jakarta.ejb.LocalBean;
+
+/**
+ *
+ * @author Student
+ */
+@Singleton
+@LocalBean
+public class ResultStorage {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
